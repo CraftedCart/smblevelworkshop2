7f676cb199f9a9b983607272ceb37fb27405e084
CraftedCart <CraftedCart@users.noreply.github.com>
2017-07-05 15:07:20 +0100

    Upped camera movement speed

--- include/widget/ViewportWidget.hpp
+++ include/widget/ViewportWidget.hpp
@@ -33,7 +33,7 @@ namespace WS2 {
             glm::vec3 forward;
             glm::vec3 right;
             glm::vec3 up;
-            float posSpeed = 16.0f;
+            float posSpeed = 24.0f;
             float rotSpeed = 0.01f;
             EnumCameraNav cameraNavMode = EnumCameraNav::NAV_FIXED;
 
