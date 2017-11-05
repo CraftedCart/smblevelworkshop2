5247ce459387a36205a94de207a218779a668af0
CraftedCart <CraftedCart@users.noreply.github.com>
2017-11-05 17:45:08 +0000

    Removed "Processing node" log output

--- ws2common/src/ws2common/model/ModelLoader.cpp
+++ ws2common/src/ws2common/model/ModelLoader.cpp
@@ -54,7 +54,7 @@ namespace WS2Common {
                         QVector<WS2Common::Resource::ResourceMesh*> &meshVector,
                         QVector<Resource::AbstractResource*> *resources
                         ) {
-                    qDebug() << "Processing node" << node->mName.C_Str() << node->mNumMeshes;
+                    //qDebug() << "Processing node" << node->mName.C_Str() << node->mNumMeshes;
 
                     QVector<WS2Common::Model::MeshSegment*> segments; //Will contain all mesh segments for each material of this node's mesh
                     //Process this node's mesh segments
